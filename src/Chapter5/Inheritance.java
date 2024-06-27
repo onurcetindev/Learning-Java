c345cb318f1750349ad722939313758c5965754b
Onur Cetin <onucetin003@gmail.com>
2024-06-27 18:48:20 +0300

    OOP Part 1 finished

--- src/Chapter5/Inheritance.java
+++ src/Chapter5/Inheritance.java
@@ -1,4 +1,95 @@
 package Chapter5;
 
+import java.security.PublicKey;
+
 public class Inheritance {
+    public static void main(String[] args) {
+
+        Manager manager = new Manager("Onur Çetin",30000,"IT Dev.",25);
+        manager.changeDepartment("DevOps");
+        manager.work();
+        manager.zamYap(200);
+        manager.showInfo();
+
+    }
+}
+
+//---------------------EMPLOYEE-------------------
+class Employee{
+    private String name;
+    private int salary;
+    private String department;
+
+    public Employee(String name, int salary, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void work(){
+        System.out.println("Employee working ");
+    }
+    public void showInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+        System.out.println("Department: " + department);
+
+    }
+    public void changeDepartment(String newDepartment){
+        System.out.println("Changing department ... ");
+        this.department = newDepartment;
+        System.out.println("Department changed to " + newDepartment);
+    }
+}
+
+//---------------MANAGER-------------------------------
+class Manager extends Employee{ // Subclass
+
+    private int sorumluKisiSayisi;
+
+    public Manager(String name, int salary, String department,int sorumluKisiSayisi) {
+        super(name, salary, department);
+        this.sorumluKisiSayisi = sorumluKisiSayisi;
+    }
+    public void zamYap(int zam){
+        System.out.println("Çalışanlara zam yapıldı");
+        System.out.println("Zam miktari : " +zam);
+    }
+    //OVERRIDE
+    public void showInfo(){
+        /*
+
+        System.out.println("Name: " + getName());
+        System.out.println("Salary: " + getSalary());
+        System.out.println("Department: " + getDepartment());
+
+        */
+        super.showInfo();
+        System.out.println("Sorumlu kişi sayisi : " + this.sorumluKisiSayisi);
+    }
+
 }
--- src/Chapter5/example/TrainingProgram.java
+++ src/Chapter5/example/TrainingProgram.java
@@ -1,4 +1,161 @@
 package Chapter5.example;
 
+import Chapter1.ScannerObject;
+
+import java.util.Scanner;
+
 public class TrainingProgram {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("İdman programına hoşgeldiniz ...");
+
+        String idmanlar = " Geçerli hareketler \n" +
+                            "1- Burpee \n"+
+                            "2- Pushup \n" +
+                            "3- Situp \n" +
+                            "4- Squad ";
+        System.out.printf(idmanlar);
+        System.out.println("Bir idman oluşturunuz ");
+
+        System.out.print("Burpee sayısı giriniz : ");
+        int burpee = scanner.nextInt();
+        System.out.print("Pushup sayısı giriniz : ");
+        int pushup = scanner.nextInt();
+        System.out.print("Situp sayısı giriniz : ");
+        int situp = scanner.nextInt();
+        System.out.print("Squad sayısı giriniz : ");
+        int squad = scanner.nextInt();
+        scanner.nextLine();
+
+        Idman idman = new Idman(burpee, pushup, situp, squad);
+        System.out.println("İdmanınınz başlıyor ..... ");
+
+        while (idman.isFinish() == false){
+            System.out.println(" Hareket türü :\n");
+            String tur = scanner.nextLine();
+            System.out.println(" Hareket sayısı :\n");
+            int sayi = scanner.nextInt();
+            scanner.nextLine();
+            idman.hareketYap(tur, sayi);
+
+
+        }
+
+
+
+    }
+
+
+}
+
+class Idman{
+    private int burpee_number;
+    private int pushup_number;
+    private int situp_number;
+    private int squad_number;
+
+    public Idman(int burpere_number, int pushup_number, int situp_number, int squad_number) {
+        this.burpee_number = burpere_number;
+        this.pushup_number = pushup_number;
+        this.situp_number = situp_number;
+        this.squad_number = squad_number;
+    }
+    public void hareketYap(String hareketTuru,int number){
+        if (hareketTuru.equals("Burpee")){
+            burpeeYap(number);
+        }else if (hareketTuru.equals("Pushup")){
+            pushupYap(number);
+        }else if (hareketTuru.equals("Situp")){
+            situpYap(number);
+        } else if (hareketTuru.equals("Squad")) {
+            squadYap(number);
+        }else{
+            System.out.printf("Geçersiz hareket");
+        }
+
+    }
+
+    public int getBurpere_number() {
+        return burpee_number;
+    }
+
+    public void setBurpere_number(int burpere_number) {
+        this.burpee_number = burpere_number;
+    }
+
+    public int getPushup_number() {
+        return pushup_number;
+    }
+
+    public void setPushup_number(int pushup_number) {
+        this.pushup_number = pushup_number;
+    }
+
+    public int getSitup_number() {
+        return situp_number;
+    }
+
+    public void setSitup_number(int situp_number) {
+        this.situp_number = situp_number;
+    }
+
+    public int getSquad_number() {
+        return squad_number;
+    }
+
+    public void setSquad_number(int squad_number) {
+        this.squad_number = squad_number;
+    }
+    public  void burpeeYap(int number){
+        if (burpee_number == 0){
+            System.out.printf("Yapacak burpee kalmadı");
+        } else if (number > burpee_number) {
+            System.out.printf("Hedelediğinden fazla burpee yaptın. Tebrikler.");
+            burpee_number = 0;
+            System.out.printf("Kalan burpee sayısı : " + burpee_number);
+        }else{
+            burpee_number -= number;
+            System.out.printf("Kalan burpee sayısı : " + burpee_number);
+        }
+    }
+    public  void pushupYap(int number){
+        if (pushup_number == 0){
+            System.out.printf("Yapacak pushup kalmadı");
+        } else if (number > pushup_number) {
+            System.out.printf("Hedelediğinden fazla pushup yaptın. Tebrikler.");
+            pushup_number = 0;
+            System.out.printf("Kalan pushup sayısı : " + pushup_number);
+        }else{
+            pushup_number -= number;
+            System.out.printf("Kalan pushup sayısı : " + pushup_number);
+        }
+    }
+    public  void situpYap(int number){
+        if (situp_number == 0){
+            System.out.printf("Yapacak situp kalmadı");
+        } else if (number > situp_number) {
+            System.out.printf("Hedelediğinden fazla situp yaptın. Tebrikler.");
+            situp_number = 0;
+            System.out.printf("Kalan situp sayısı : " + situp_number);
+        }else{
+            situp_number -= number;
+            System.out.printf("Kalan situp sayısı : " + situp_number);
+        }
+    }
+    public  void squadYap(int number){
+        if (squad_number == 0){
+            System.out.printf("Yapacak squad kalmadı");
+        } else if (number > squad_number) {
+            System.out.printf("Hedelediğinden fazla squad yaptın. Tebrikler.");
+            squad_number = 0;
+            System.out.printf("Kalan squad sayısı : " + squad_number);
+        }else{
+            squad_number -= number;
+            System.out.printf("Kalan squad sayısı : " + squad_number);
+        }
+    }
+    public boolean isFinish(){
+        return (burpee_number == 0) &&(situp_number == 0) &&(squad_number == 0) && (pushup_number == 0);
+    }
 }
